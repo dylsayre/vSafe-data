59e836323171acf8481aac95a07b76835f4ba967
Sayviour <dylan.j.sayre@gmail.com>
2026-01-21 15:59:05 -0500

    removing deprecated versbose logging

--- scripts/train.py
+++ scripts/train.py
@@ -311,7 +311,7 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode='min', factor=0.5, patience=5, verbose=True
+        optimizer, mode='min', factor=0.5, patience=5
     )
 
     # Training loop
